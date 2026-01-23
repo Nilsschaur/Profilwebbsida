55c9cbc57c66c286ffed89a14cf69c86b3f323e5
Nilsschaur <nils.schaur@angeskola.se>
2026-01-23 09:18:39 +0000

    [Add]Dark_Mode

---
+++ Profilwebbsida/index.js
@@ -0,0 +1,20 @@
+function toggleDarkMode(){
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("theme", "dark");
+    }else{
+        localStorage.setItem("theme", "light");
+    }
+}
+
+document.querySelector("#darkBtn").addEventListener("click", toggleDarkMode);
+
+function loadTheme(){
+    const theme = localStorage.getItem("theme");
+    if(theme === "dark"){
+        document.body.classList.add("dark");
+    }
+}
+
+loadTheme();
